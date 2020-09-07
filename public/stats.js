13a22add648da7bfc5e66fe9d26e161104020caf
Porkchopsss <pokoshih@gmail.com>
2020-09-07 17:04:25 +1000

    deploying

--- public/stats.js
+++ public/stats.js
@@ -79,7 +79,7 @@ function populateChart(data) {
       datasets: [
         {
           label: "Bench Press",
-          data: pounds,
+          data: kilos,
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
             "rgba(54, 162, 235, 0.2)",
